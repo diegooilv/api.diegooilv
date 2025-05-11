3abcf73d72486e6d3ced5eb903247497ccfc9cee
DiegoOliv07 <diego.oliv2007@gmail.com>
2025-05-10 22:16:29 -0300

    feat: rota de teste

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import usersRouter from "./users.router.js";
 import tabelaRouter from "./tabela.router.js";
+import testeRouter from "./teste.router.js"
 
 const router = Router();
 
+router.use("/teste", testeRouter)
 router.use("/users", usersRouter);
 router.use("/tabela", tabelaRouter);
 
